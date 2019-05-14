59bf5cc8728c5dae50864626240634afe8e3f5b6
pengjie5 <pengjie5@jd.com>
2019-05-14 13:07:12 +0800

    ADD: CALL

---
+++ Practice/call-apply-bind.js
@@ -0,0 +1,40 @@
+Function.prototype._call_ = function(ctx, ...args) {
+    // 核心就是改变被调用 fn 的指向
+    // 即让 ctx 调用 fn
+    // 获取 fn
+    ctx = ctx || window;
+    ctx.fn = this;
+    let result = ctx.fn(...args);
+    delete ctx.fn;
+    return result;
+};
+
+function test(num) {
+    console.log(this, num);
+}
+
+test._call_({ a: 1 }, 2);
+
+Function.prototype._apply_ = function(ctx, argArr = []) {
+    // 核心就是改变被调用 fn 的指向
+    // 即让 ctx 调用 fn
+    // 获取 fn
+    ctx = ctx || window;
+    ctx.fn = this;
+    let result = ctx.fn(...argArr);
+    delete ctx.fn;
+    return result;
+};
+
+Function.prototype._bind_ = function(ctx) {
+    ctx.fn = this;
+    return function(...args) {
+        ctx.fn(...args);
+    };
+};
+
+function test2(num) {
+    console.log(this, num);
+}
+
+test2._bind_({ b: 2 })(1, 2, 3);
