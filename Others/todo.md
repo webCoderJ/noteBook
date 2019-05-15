799e3fa1d3de1251f1e150b2e832ae197e288e4a
pengjie <pengjie5@jd.com>
2019-05-15 22:29:08 +0800

    GEN

--- Others/todo.md
+++ Others/todo.md
@@ -4,11 +4,35 @@
 | ------------------------------------------------- | ------------ | ------ |
 | [白帽子Web安全]                                   | 本周         | `Done` |
 | Babel                                             | 4.1          | `Done` |
-| Vue源码 <https://github.com/answershuto/learnVue> | 4.01 - 4.15  |        |
-| 数据结构与算法                                    | 4.16 - 4.25  |        |
+| Vue源码 <https://github.com/answershuto/learnVue> | 4.01 - 4.15  | `Done` |
+| 数据结构与算法                                    | 4.16 - 4.25  | `60%`  |
 | NodeJs egg、mongoDB、MySQL、GraphQL               | 4.25 - 5.15  |        |
 | 前端架构的学习、JS/CSS基础知识、编译原理、Http    | 5.15 - 6.01  |        |
 | Flutter + Fish Redux、redux、rxjs                 | 6.01 - 6.10  |        |
 
 每天两道算法题
+
+CSS
+
+- BFC 
+- 适配问题
+  - rem flexable
+  - vm vh
+
+webpack
+
+- 各种loader的配置以及项目搭建配置
+
+node
+
+- koa
+- express
+- egg
+
+vue
+
+-  router
+- vuex
+
+面试题各种刷
 
---
+++ Practice/genarator+async.js
@@ -0,0 +1,96 @@
+function* idGenarator() {
+    for (let i = 0; i >= 0; i++) {
+        yield i;
+    }
+}
+
+let id = idGenarator();
+
+// console.log(id.next());
+
+function* yieldVal() {
+    let a = yield 1;
+    console.log("1 - resolved", a);
+    yield a;
+}
+
+let nextParam = yieldVal();
+// v8会忽略第一次的next参数，因为next的参数是作为上一次yield的返回值
+// console.log(nextParam.next());
+// console.log(nextParam.next(1));
+
+// throw
+
+function* throwGen() {
+    try {
+        yield 1;
+    } catch (e) {
+        console.log("内部捕获", e);
+    }
+}
+
+// let i = throwGen();
+// i.next();
+
+// try {
+//     i.throw("generator-err 1")
+//     // i.throw("generator-err 2")
+// } catch (e) {
+//     console.log("外部捕获", e);
+// }
+
+function* gReturn() {
+    yield 1;
+    yield 2;
+    yield 3;
+}
+
+// let j = gReturn();
+// console.log(j.next());
+// console.log(j.return(-1));
+// console.log(j.next());
+
+function* gen2() {
+    yield 4;
+    yield* gReturn();
+    yield 5;
+    return console.log("done");
+}
+
+// let gen_2 = gen2();
+// console.log(gen_2.next());
+// console.log(gen_2.next());
+// console.log(gen_2.next());
+// console.log(gen_2.next());
+
+// function* gen_flat_arr(arr) {
+//     if (Array.isArray(arr)) {
+//         for (let i = 0; i < arr.length; i++) {
+//             yield* gen_flat_arr(arr[i]);
+//         }
+//     } else {
+//         yield arr;
+//     }
+// }
+
+// for (let x of gen_flat_arr([1, 2, [3, 4]])) {
+//     console.log(x);
+// }
+
+// let obj = {
+//     * genKey(){
+//         yield 1;
+//     }
+// }
+
+// this
+function* foo(){
+    this.a = 1;
+    this.b = 2;
+    console.log(this)
+}
+
+let a = foo();
+console.log(a.next());
+console.log(a.next());
+console.log(a.next());
